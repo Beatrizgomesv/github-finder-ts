eb0814574b6288982cee2a5e771b1b0740f40b47
Beatriz Gomes <beatrizg00@hotmail.com>
2022-06-28 12:21:06 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { RepositoryList } from "./components/RepositoryList.jsx";
+import "./styles/global.scss";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <RepositoryList></RepositoryList>
+   
+    </div>
+  );
+}
+
+export default App;
